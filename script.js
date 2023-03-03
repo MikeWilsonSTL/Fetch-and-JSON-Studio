32598745f2763234765129c583a8256759e9fba7
Mike Wilson <mikewilsonstl@gmail.com>
2023-03-02 19:18:05 -0600

    Refactored to use async/await. Added error checking.

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-window.addEventListener('load', function () {
-    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function (response) {
-        response.json().then(function (json) {
-            json.sort((b, a) => {
-                return a.hoursInSpace - b.hoursInSpace;
-            });
-            container.innerHTML += `<h3>Total Astronauts: ${json.length}</h3>`;
-            for (let x in json) {
-                if (json[x].active === true) {
-                    json[x].active = `<span style="color:green;font-weight:bold;">${json[x].active}</span>`;
-                }
-                container.innerHTML += `<div class="astronaut">
-                      <div class="bio">
-                      <h3>${json[x].firstName} ${json[x].lastName}</h3>
-                      <ul>
-                          <li>Hours in space: ${json[x].hoursInSpace}</li>
-                          <li>Active: ${json[x].active}</li>
-                          <li>Skills: ${json[x].skills.join(', ')}</li>
-                      </ul>
-                      </div>
-                      <img class="avatar" src="${json[x].picture}"/>
-                  </div>`;
-            }
+window.addEventListener('load', async function () {
+    try {
+        const response = await fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+        const data = await response.json();
+        data.sort((b, a) => {
+            return a.hoursInSpace - b.hoursInSpace;
         });
-    });
+
+        container.innerHTML += `<h3>Total Astronauts: ${data.length}</h3>`;
+
+        for (let x in data) {
+            if (data[x].active === true) {
+                data[x].active = `<span style="color:green;font-weight:bold;">${data[x].active}</span>`;
+            }
+            container.innerHTML += `<div class="astronaut">
+                <div class="bio">
+                <h3>${data[x].firstName} ${data[x].lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${data[x].hoursInSpace}</li>
+                    <li>Active: ${data[x].active}</li>
+                    <li>Skills: ${data[x].skills.join(', ')}</li>
+                </ul>
+                </div>
+                <img class="avatar" src="${data[x].picture}"/>
+            </div>`;
+        }
+    }
+    catch {
+        container.innerHTML = `<h1 style='color:red;font-size: 5em;'>ERROR</h1>
+        <h2 style='color:red;font-size: 2em;'>The API URL is likely malformed.</h2>`;
+        console.error("ERROR: The API URL is likely malformed.");
+    }
 });
